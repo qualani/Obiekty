fcd6146b80554adda135f3c17c91a740c1408dd0
paula s <qua.lani@gmail.com>
2013-03-18 18:00:56 +0100

    Programowanie obiektowe

---
+++ Lista 2 IntStream i Leniwa/IntStream.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class IntStream
+    {
+        protected Random rand = new Random();
+        protected int strumien = -1;
+
+        virtual public int Next()
+        {
+            if (!Eos()) strumien++;
+            return strumien;
+        }
+
+        virtual public bool Eos()
+        {
+            if (strumien >= 1000) return true; else return false;
+        }
+
+        public void Reset()
+        {
+            strumien = -1;
+        }
+    }
+
+    class PrimeStream : IntStream
+    {
+        bool isPrime(int x)
+        {
+            if (x < 2) return false;
+            if (x == 2) return true;
+            if (x % 2 == 0) return false;
+            for (int i = 3; i <= (x / 2 + 1); i = i + 2) if (x % i == 0) return false;
+            return true;
+        }
+
+        override public int Next()
+        {
+            if (!Eos()) strumien = nextPrime();
+            return strumien;
+        }
+
+        override public bool Eos()
+        {
+            if (strumien + 1 < 0) return true; else return false;
+        }
+
+        int nextPrime()
+        {
+            int szukana = strumien + 1;
+            while (!isPrime(szukana))
+            {
+                szukana++;
+            }
+            return szukana;
+        }
+    }
+
+    class RandomStream : IntStream
+    {
+        override public int Next()
+        {
+            strumien = rand.Next(0, 1000);
+            return strumien;
+        }
+
+        override public bool Eos()
+        {
+            return false;
+        }
+    }
+
+    class RandomWordStream
+    {
+        RandomStream rand = new RandomStream();
+        PrimeStream prime = new PrimeStream();
+        int strumien_prime;
+
+        public bool Eos()
+        {
+            return prime.Eos();
+        }
+
+        public void Next()
+        {
+            strumien_prime = prime.Next();
+            for (int i = 0; i < strumien_prime; i++)
+            {
+                int losowy = rand.Next();
+                Console.Write(losowy % 10);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Lista 2 IntStream i Leniwa/Leniwa.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class ListaLeniwa
+    {
+        protected int[] elementy;
+        protected int rozmiar = 0;
+        Random rand = new Random();
+
+        virtual public int Element(int x)
+        {
+            if (rozmiar < x)
+            {
+                Array.Resize(ref elementy, x + 1);
+                for (int i = rozmiar + 1; i <= x; i++)
+                {
+                    elementy[i] = rand.Next(0, 10000);
+                }
+                rozmiar = x;
+            }
+            return elementy[x];
+        }
+
+        public int Size()
+        {
+            return rozmiar;
+        }
+    }
+
+    class Pierwsze : ListaLeniwa
+    {
+        bool isPrime(int x)
+        {
+            if (x <= 3) return true;
+            if (x % 2 == 0) return false;
+            for (int i = 3; i <= (x / 2 + 1); i = i + 2) if (x % i == 0) return false;
+            return true;
+        }
+
+        override public int Element(int x)
+        {
+            if (rozmiar == 0)
+            {
+                Array.Resize(ref elementy, 2);
+                elementy[1] = 2;
+                rozmiar++;
+            }
+            if (rozmiar < x)
+            {
+                Array.Resize(ref elementy, x + 1);
+                for (int i = rozmiar + 1; i <= x; i++)
+                {
+                    int szukana = elementy[i - 1] + 1;
+                    while (!isPrime(szukana)) szukana++;
+                    elementy[i] = szukana;
+                    rozmiar++;
+                }
+            }
+            return elementy[x];
+        }
+    }
+}
